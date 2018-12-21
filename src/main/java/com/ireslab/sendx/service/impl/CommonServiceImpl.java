bc5efac6c8e9ed82c5f5f8266b07fdaa325e99f6
abhinav-ireslab <abhinavgarg3@gmail.com>
2018-12-21 16:25:06 +0530

    Update CommonServiceImpl.java

--- src/main/java/com/ireslab/sendx/service/impl/CommonServiceImpl.java
+++ src/main/java/com/ireslab/sendx/service/impl/CommonServiceImpl.java
@@ -1455,7 +1455,7 @@ public class CommonServiceImpl  implements CommonService {
 		
 		}	
 			
-		String invoice = "\r<!DOCTYPE html>\r\n" + 
+		String invoice = "<!DOCTYPE html>\r\n" + 
 				"<html>\r\n" + 
 				"\r\n" + 
 				"<head>\r\n" + 
@@ -1697,7 +1697,7 @@ public class CommonServiceImpl  implements CommonService {
 				"</html>";
 		//System.out.println("invoice 1 ---------- "+invoice);
 		
-		String receipt = "\r<!DOCTYPE html>\r\n" + "<html>\r\n" + "\r\n" + "<head>\r\n"
+		String receipt = "<!DOCTYPE html>\r\n" + "<html>\r\n" + "\r\n" + "<head>\r\n"
 				+ "    <meta charset=\"utf-8\" />\r\n"
 				+ "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\" />\r\n"
 				+ "    <title>Receipt</title>\r\n"
@@ -2526,7 +2526,7 @@ public class CommonServiceImpl  implements CommonService {
 		
 		}
 			
-		String invoice = "\r<!DOCTYPE html>\r\n" + 
+		String invoice = "<!DOCTYPE html>\r\n" + 
 				"<html>\r\n" + 
 				"\r\n" + 
 				"<head>\r\n" + 
@@ -2770,7 +2770,7 @@ public class CommonServiceImpl  implements CommonService {
 		
 		//System.out.println("invoice 2 ------------------------- "+invoice);
 		
-		String receipt = "\r<!DOCTYPE html>\r\n" + "<html>\r\n" + "\r\n" + "<head>\r\n"
+		String receipt = "<!DOCTYPE html>\r\n" + "<html>\r\n" + "\r\n" + "<head>\r\n"
 				+ "    <meta charset=\"utf-8\" />\r\n"
 				+ "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\" />\r\n"
 				+ "    <title>Receipt</title>\r\n"
@@ -2808,7 +2808,7 @@ public class CommonServiceImpl  implements CommonService {
 				+ "        .text-left {\r\n" + "            text-align: left;\r\n" + "        }\r\n" + "\r\n"
 				+ "        .billTable {\r\n" + "            width: 100%;\r\n" + "        }\r\n" + "\r\n"
 				+ "        .billTable th,\r\n" + "        .billTable td {\r\n" + "            padding: 8px 5px;\r\n"
-				+ "        }\r\n" + "\r\n" + "        .noMargin {\r\n" + "            margin: 0px;\r\n"
+				+ "        }\r\n" + "\r\nerror parsing HTTP 404 response body: invalid character '<' looking for beginning of value: "<!doctype" + "        .noMargin {\r\n" + "            margin: 0px;\r\n"
 				+ "        }\r\n" + "\r\n" + "        .address {\r\n" + "            width: 300px;\r\n"
 				+ "            word-wrap: break-word;\r\n" + "        }\r\n" + "    </style>\r\n" + "</head>\r\n"
 				+ "\r\n" + "<body>\r\n" + "    <div class=\"container\">\r\n" + "        <div class=\"header\">\r\n"
